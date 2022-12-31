6697972f78b21604ebc4d3dd37d947f9ec219dfd
barteqcz <118764028+barteqcz@users.noreply.github.com>
2022-12-31 14:25:20 +0100

    Update micrords.c

--- src/micrords.c
+++ src/micrords.c
@@ -132,7 +132,7 @@ int main(int argc, char **argv) {
 	char control_pipe[51];
 	struct rds_params_t rds_params = {
 		.ps = "MicroRDS",
-		.rt = "MiniRDS: A lightweight RDS encoder for Linux",
+		.rt = "MicroRDS: A lightweight RDS encoder for Linux",
 		.pi = 0x1000
 	};
 	char callsign[5];
