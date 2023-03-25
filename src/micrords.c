0770ef3115b29e7487e1c4f010313a0c13062e4d
barteqcz <118764028+barteqcz@users.noreply.github.com>
2023-03-25 22:21:28 +0100

    Update micrords.c

--- src/micrords.c
+++ src/micrords.c
@@ -67,17 +67,17 @@ static void show_help() {
 		"    -s, --ps           Program Service name\n"
 		"    -r, --rt           Radio Text\n"
 		"    -p, --pty          Program Type\n"
-		"    -T, --tp           Traffic Program\n"
+		"    -t, --tp           Traffic Program\n"
 #ifdef RBDS
-		"    -A, --af           Alternative Frequency [FM/MF]\n"
+		"    -a, --af           Alternative Frequency [FM/MF]\n"
 #else
-		"    -A, --af           Alternative Frequency [FM/LF/MF] (more than one AF may be passed)\n"
+		"    -a, --af           Alternative Frequency [FM/LF/MF] (more than one AF may be passed)\n"
 #endif
 		"    -P, --ptyn         Program Type Name\n"
 #ifdef RBDS
 		"    -S, --callsign     FCC callsign to calculate the PI code from (overrides -i,--pi)\n"
 #endif
-		"    -C, --ctl          FIFO control pipe\n"
+		"    -c, --ctl          FIFO control pipe\n"
 		"    -h, --help         Show this help text and exit\n"
 		"\n"
 	       );
