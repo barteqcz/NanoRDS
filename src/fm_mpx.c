6d7485045f17e00ecd82d707947ba308d962be15
barteqcz <118764028+barteqcz@users.noreply.github.com>
2023-03-25 20:02:45 +0100

    Update fm_mpx.c

--- src/fm_mpx.c
+++ src/fm_mpx.c
@@ -55,8 +55,13 @@ void set_carrier_volume(uint8_t carrier, uint8_t new_volume) {
 
 void fm_mpx_init(uint32_t sample_rate) {
 	/* initialize the subcarrier oscillators */
+	#ifdef STEREO
 	osc_init(&osc_19k, sample_rate, 19000.0f);
 	osc_init(&osc_57k, sample_rate, 57000.0f);
+	#else
+	osc_init(&osc_19k, sample_rate, 00000.0f);
+	osc_init(&osc_57k, sample_rate, 57000.0f);
+	#endif
 }
 
 void fm_rds_get_frames(float *outbuf, size_t num_frames) {
