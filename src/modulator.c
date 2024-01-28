c5b75b0f74299613d17b66f79c70cf66b05014be
Bartek <118764028+barteqcz@users.noreply.github.com>
2024-01-28 11:31:09 +0100

    Update modulator.c

--- src/modulator.c
+++ src/modulator.c
@@ -1,26 +1,5 @@
-/*
- * mpxgen - FM multiplex encoder with Stereo and RDS
- * Copyright (C) 2021 Anthony96922
- *
- * This program is free software: you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation, either version 3 of the License, or
- * (at your option) any later version.
- *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License
- * along with this program.  If not, see <http://www.gnu.org/licenses/>.
- */
-
 #include "common.h"
 #include "rds.h"
-#ifdef RDS2
-#include "rds2.h"
-#endif
 #include "fm_mpx.h"
 #include "waveforms.h"
 #include "modulator.h"
@@ -80,15 +59,7 @@ float get_rds_sample(uint8_t stream_num) {
 
 	if (rds->sample_count == SAMPLES_PER_BIT) {
 		if (rds->bit_pos == BITS_PER_GROUP) {
-#ifdef RDS2
-			if (stream_num > 0) {
-				get_rds2_bits(stream_num, rds->bit_buffer);
-			} else {
-				get_rds_bits(rds->bit_buffer);
-			}
-#else
 			get_rds_bits(rds->bit_buffer);
-#endif
 			rds->bit_pos = 0;
 		}
 
