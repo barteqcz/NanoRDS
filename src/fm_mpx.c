d8d9429162553aae760731f7096fd17a03fdf6aa
Bartek <118764028+barteqcz@users.noreply.github.com>
2024-01-28 11:16:01 +0100

    Update fm_mpx.c

--- src/fm_mpx.c
+++ src/fm_mpx.c
@@ -1,27 +1,5 @@
-/*
- * mpxgen - FM multiplex encoder with Stereo and RDS
- * Copyright (C) 2019 Anthony96922
- *
- * This program is free software: you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation, either version 3 of the License, or
- * (at your option) any later version.
- *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License
- * along with this program.  If not, see <http://www.gnu.org/licenses/>.
- */
-
 #include "common.h"
-
 #include "rds.h"
-#ifdef RDS2
-#include "rds2.h"
-#endif
 #include "fm_mpx.h"
 #include "osc.h"
 
@@ -32,11 +10,6 @@
  */
 static struct osc_t osc_19k;
 static struct osc_t osc_57k;
-#ifdef RDS2
-static struct osc_t osc_67k;
-static struct osc_t osc_71k;
-static struct osc_t osc_76k;
-#endif
 
 static float mpx_vol;
 
@@ -49,12 +22,6 @@ void set_output_volume(uint8_t vol) {
 static float volumes[] = {
 	0.09f, /* pilot tone: 9% */
 	0.09f, /* RDS: 4.5% modulation */
-#ifdef RDS2
-	/* RDS2 */
-	0.09f,
-	0.09f,
-	0.09f
-#endif
 };
 
 void set_carrier_volume(uint8_t carrier, uint8_t new_volume) {
@@ -71,11 +38,6 @@ void fm_mpx_init(uint32_t sample_rate) {
 	/* initialize the subcarrier oscillators */
 	osc_init(&osc_19k, sample_rate, 19000.0f);
 	osc_init(&osc_57k, sample_rate, 57000.0f);
-#ifdef RDS2
-	osc_init(&osc_67k, sample_rate, 66500.0f);
-	osc_init(&osc_71k, sample_rate, 71250.0f);
-	osc_init(&osc_76k, sample_rate, 76000.0f);
-#endif
 }
 
 void fm_rds_get_frames(float *outbuf, size_t num_frames) {
@@ -87,22 +49,11 @@ void fm_rds_get_frames(float *outbuf, size_t num_frames) {
 
 		/* Pilot tone for calibration */
 		out += osc_get_cos(&osc_19k) * volumes[0];
-
 		out += osc_get_cos(&osc_57k) * get_rds_sample(0) * volumes[1];
-#ifdef RDS2
-		out += osc_get_cos(&osc_67k) * get_rds_sample(1) * volumes[2];
-		out += osc_get_cos(&osc_71k) * get_rds_sample(2) * volumes[3];
-		out += osc_get_cos(&osc_76k) * get_rds_sample(3) * volumes[4];
-#endif
 
 		/* update oscillator */
 		osc_update_pos(&osc_19k);
 		osc_update_pos(&osc_57k);
-#ifdef RDS2
-		osc_update_pos(&osc_67k);
-		osc_update_pos(&osc_71k);
-		osc_update_pos(&osc_76k);
-#endif
 
 		/* clipper */
 		out = fminf(+1.0f, out);
@@ -118,9 +69,4 @@ void fm_rds_get_frames(float *outbuf, size_t num_frames) {
 void fm_mpx_exit() {
 	osc_exit(&osc_19k);
 	osc_exit(&osc_57k);
-#ifdef RDS2
-	osc_exit(&osc_67k);
-	osc_exit(&osc_71k);
-	osc_exit(&osc_76k);
-#endif
 }
