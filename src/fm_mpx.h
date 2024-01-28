8b8c606fbd56bc2affeb9aa4f66d1ce46234c55c
Bartek <118764028+barteqcz@users.noreply.github.com>
2024-01-28 11:16:20 +0100

    Update fm_mpx.h

--- src/fm_mpx.h
+++ src/fm_mpx.h
@@ -1,21 +1,3 @@
-/*
- * mpxgen - FM multiplex encoder with Stereo and RDS
- * Copyright (C) 2019 Anthony96922
- *
- * This program is free software: you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation, either version 3 of the License, or
- * (at your option) any later version.
- *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License
- * along with this program.  If not, see <http://www.gnu.org/licenses/>.
- */
-
 /* MPX */
 #define NUM_MPX_FRAMES_IN	1024
 #define NUM_MPX_FRAMES_OUT	(NUM_MPX_FRAMES_IN * 2)
@@ -27,11 +9,7 @@
 
 #define OUTPUT_SAMPLE_RATE	192000
 
-#ifdef RDS2
-#define NUM_SUBCARRIERS		5
-#else
 #define NUM_SUBCARRIERS		1
-#endif
 
 extern void fm_mpx_init(uint32_t sample_rate);
 extern void fm_rds_get_frames(float *outbuf, size_t num_frames);
