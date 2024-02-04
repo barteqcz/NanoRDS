329efbea45fd8ee450c24f5a713b3a4ffdbbd562
Bartek <118764028+barteqcz@users.noreply.github.com>
2024-02-04 11:06:28 +0100

    Remove "VOL" command redundants

--- src/fm_mpx.c
+++ src/fm_mpx.c
@@ -9,17 +9,11 @@
  */
 static struct osc_t osc_19k;
 static struct osc_t osc_57k;
-static float mpx_vol;
-
-void set_output_volume(uint8_t vol) {
-	if (vol > 100) vol = 100;
-	mpx_vol = ((float)vol / 100.0f);
-}
 
 /* initial subcarrier volumes */
 static float volumes[] = {
-	0.09f, /* Stereo pilot: 9% */
-	0.09f /* RDS modulation: 4.5% */
+        0.09f, /* Stereo pilot: 9% */
+        0.09f /* RDS modulation: 4.5% */
 };
 
 void set_carrier_volume(uint8_t carrier, uint8_t new_volume) {
@@ -44,38 +38,38 @@ void set_carrier_volume(uint8_t carrier, uint8_t new_volume) {
 
 
 void fm_mpx_init(uint32_t sample_rate) {
-	/* initialize the subcarrier oscillators */
-	osc_init(&osc_19k, sample_rate, 19000.0f);
-	osc_init(&osc_57k, sample_rate, 57000.0f);
+        /* initialize the subcarrier oscillators */
+        osc_init(&osc_19k, sample_rate, 19000.0f);
+        osc_init(&osc_57k, sample_rate, 19000.0f);
 }
 
 void fm_rds_get_frames(float *outbuf, size_t num_frames) {
-	size_t j = 0;
-	float out;
+        size_t j = 0;
+        float out;
 
-	for (size_t i = 0; i < num_frames; i++) {
-		out = 0.0f;
+        for (size_t i = 0; i < num_frames; i++) {
+                out = 0.0f;
 
-		/* Pilot tone for calibration */
-		out += osc_get_cos(&osc_19k) * volumes[0];
-		out += osc_get_cos(&osc_57k) * get_rds_sample(0) * volumes[1];
+                /* Pilot tone for calibration */
+                out += osc_get_cos(&osc_19k) * volumes[0];
+                out += osc_get_cos(&osc_57k) * get_rds_sample(0) * volumes[1];
 
-		/* update oscillator */
-		osc_update_pos(&osc_19k);
-		osc_update_pos(&osc_57k);
+                /* update oscillator */
+                osc_update_pos(&osc_19k);
+                osc_update_pos(&osc_57k);
 
-		/* clipper */
-		out = fminf(+1.0f, out);
-		out = fmaxf(-1.0f, out);
+                /* clipper */
+                out = fminf(+1.0f, out);
+                out = fmaxf(-1.0f, out);
 
-		/* adjust volume and put into both channels */
-		outbuf[j+0] = outbuf[j+1] = out * mpx_vol;
-		j += 2;
+                /* adjust volume and put into both channels */
+                outbuf[j+0] = outbuf[j+1] = out;
+                j += 2;
 
-	}
+        }
 }
 
 void fm_mpx_exit() {
-	osc_exit(&osc_19k);
-	osc_exit(&osc_57k);
+        osc_exit(&osc_19k);
+        osc_exit(&osc_57k);
 }
