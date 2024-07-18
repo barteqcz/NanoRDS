83a854b76ed1005d2b9058c92c05c15bd65bf8d9
Bartek <118764028+barteqcz@users.noreply.github.com>
2024-07-19 01:09:55 +0200

    Delete src directory

--- src/ascii_cmd.h
+++
@@ -1,5 +0,0 @@
-#define CMD_BUFFER_SIZE	160
-#define CTL_BUFFER_SIZE	(CMD_BUFFER_SIZE * 2)
-#define READ_TIMEOUT_MS	100
-
-extern void process_ascii_cmd(unsigned char *cmd);
--- src/control_pipe.h
+++
@@ -1,7 +0,0 @@
-#include <fcntl.h>
-#include <poll.h>
-#include <sys/select.h>
-
-extern int open_control_pipe(char *filename);
-extern void close_control_pipe();
-extern void poll_control_pipe();
