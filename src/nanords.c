a69ccb60f5c920613d40b0aa5bdfd4fa72058ddb
Bartek <118764028+barteqcz@users.noreply.github.com>
2026-02-08 11:15:05 +0100

    Updated the program's behavior.
    Now the program won't exit on audio device failure, it will try to reopen the device, this could be useful if the audio device on the site has a temporary problem. Earlier, the encoder would just stop and require manual intervention.

--- src/nanords.c
+++ src/nanords.c
@@ -27,25 +27,43 @@ static inline void float2char2channel(float *inbuf, char *outbuf, size_t frames)
         sample >>= 8;
         upper = sample & 255;
 
-        outbuf[k+0] = lower;
-        outbuf[k+1] = upper;
-
-        outbuf[k+2] = 0;
-        outbuf[k+3] = 0;
+        outbuf[k + 0] = lower;
+        outbuf[k + 1] = upper;
+        outbuf[k + 2] = 0;
+        outbuf[k + 3] = 0;
 
         j += 2;
         k += 4;
     }
 }
 
-static void *control_pipe_worker() {
+static void *control_pipe_worker(void *arg) {
+    (void)arg;
+
     while (!stop_rds) {
         poll_control_pipe();
         msleep(READ_TIMEOUT_MS);
     }
+
     fprintf(stderr, "Control pipe thread exiting...\n");
     close_control_pipe();
-    pthread_exit(NULL);
+    return NULL;
+}
+
+static ao_device *open_audio_device(ao_sample_format *format) {
+    ao_device *dev;
+
+    while (!stop_rds) {
+        dev = ao_open_live(ao_default_driver_id(), format, NULL);
+        if (dev) {
+            return dev;
+        }
+
+        fprintf(stderr, "Audio device unavailable, retrying...\n");
+        msleep(5000);
+    }
+
+    return NULL;
 }
 
 static void show_help() {
@@ -54,7 +72,7 @@ static void show_help() {
         " Usage: nanords [options]\n"
         "\n"
 #ifdef RBDS
-        " --pi <value>        Program Identification code or callsign (PI code will be calculated from callsign)\n"
+        " --pi <value>        Program Identification code or callsign\n"
 #else
         " --pi <value>        Program Identification code\n"
 #endif
@@ -70,180 +88,89 @@ static void show_help() {
         " --lic <value>       LIC code\n"
         " --stereo <value>    Stereo pilot volume\n"
         " --rds <value>       RDS subcarrier volume\n"
-        " --fifo <value>      FIFO control pipe path (FIFO commands are available on the project's website)\n"
+        " --fifo <value>      FIFO control pipe path\n"
         " --help              Show this help text and exit\n"
         "\n"
     );
 }
 
 int main(int argc, char **argv) {
-    char control_pipe[51];
+    char control_pipe[51] = {0};
+
     struct rds_params_t rds_params = {
         .ps = "NanoRDS",
         .rt = "NanoRDS - software RDS encoder for Linux",
         .pi = 0x1000
     };
 
-    float *mpx_buffer;
-    float *out_buffer;
-    char *dev_out;
+    float *mpx_buffer = NULL;
+    float *out_buffer = NULL;
+    char  *dev_out    = NULL;
 
-    int r;
     size_t frames;
+    int r;
 
-    SRC_STATE *src_state;
+    SRC_STATE *src_state = NULL;
     SRC_DATA src_data;
 
-    ao_device *device;
+    ao_device *device = NULL;
     ao_sample_format format;
 
+    pthread_t control_thread;
     pthread_attr_t attr;
-    pthread_t control_pipe_thread;
-    pthread_mutex_t control_pipe_mutex = PTHREAD_MUTEX_INITIALIZER;
-    pthread_cond_t control_pipe_cond;
-
-    memset(control_pipe, 0, 51);
+    int control_thread_running = 0;
 
     for (int i = 1; i < argc; i++) {
-        if (strcmp(argv[i], "--pi") == 0) {
-            if (i + 1 < argc) {
+        if (strcmp(argv[i], "--pi") == 0 && i + 1 < argc) {
 #ifdef RBDS
-                if (argv[i + 1][0] == 'K' || argv[i + 1][0] == 'W' ||
-                    argv[i + 1][0] == 'k' || argv[i + 1][0] == 'w') {
-                    rds_params.pi = callsign2pi((unsigned char *)argv[i + 1]);
-                } else
+            if (argv[i + 1][0] == 'K' || argv[i + 1][0] == 'W' ||
+                argv[i + 1][0] == 'k' || argv[i + 1][0] == 'w') {
+                rds_params.pi = callsign2pi((unsigned char *)argv[i + 1]);
+            } else
 #endif
-                    rds_params.pi = strtoul(argv[i + 1], NULL, 16);
-                i++;
-            } else {
-                printf("--pi needs an argument\n");
-                return 1;
-            }
-        } else if (strcmp(argv[i], "--ps") == 0) {
-            if (i + 1 < argc) {
-                memcpy(rds_params.ps, xlat((unsigned char *)argv[i + 1]), 8);
-                i++;
-            } else {
-                printf("--ps needs an argument\n");
-                return 1;
-            }
-        } else if (strcmp(argv[i], "--rt") == 0) {
-            if (i + 1 < argc) {
-                memcpy(rds_params.rt, xlat((unsigned char *)argv[i + 1]), 64);
-                i++;
-            } else {
-                printf("--rt needs an argument\n");
-                return 1;
-            }
-        } else if (strcmp(argv[i], "--pty") == 0) {
-            if (i + 1 < argc) {
-                rds_params.pty = strtoul(argv[i + 1], NULL, 10);
-                i++;
-            } else {
-                printf("--pty needs an argument\n");
-                return 1;
-            }
-        } else if (strcmp(argv[i], "--ptyn") == 0) {
-            if (i + 1 < argc) {
-                memcpy(rds_params.ptyn, xlat((unsigned char *)argv[i + 1]), 8);
-                i++;
-            } else {
-                printf("--ptyn needs an argument\n");
-                return 1;
-            }
-        } else if (strcmp(argv[i], "--ms") == 0) {
-            if (i + 1 < argc) {
-                rds_params.ms = strtoul(argv[i + 1], NULL, 10);
-                i++;
-            } else {
-                printf("--ms needs an argument\n");
-                return 1;
-            }
-        } else if (strcmp(argv[i], "--tp") == 0) {
-            if (i + 1 < argc) {
-                rds_params.tp = strtoul(argv[i + 1], NULL, 10);
-                i++;
-            } else {
-                printf("--tp needs an argument\n");
-                return 1;
-            }
-        } else if (strcmp(argv[i], "--di") == 0) {
-            if (i + 1 < argc) {
-                rds_params.di = strtoul(argv[i + 1], NULL, 10);
-                i++;
-            } else {
-                printf("--di needs an argument\n");
-                return 1;
-            }
-        } else if (strcmp(argv[i], "--af") == 0) {
-            if (i + 1 < argc) {
-                if (add_rds_af(&rds_params.af, strtof(argv[i + 1], NULL))) {
-                    return 1;
-                }
-                i++;
-            } else {
-                printf("--af needs an argument\n");
-                return 1;
-            }
-        } else if (strcmp(argv[i], "--ecc") == 0) {
-            if (i + 1 < argc) {
-                rds_params.ecc = strtoul(argv[i + 1], NULL, 16);
-                i++;
-            } else {
-                printf("--ecc needs an argument\n");
-                return 1;
-            }
-        } else if (strcmp(argv[i], "--lic") == 0) {
-            if (i + 1 < argc) {
-                rds_params.lic = strtoul(argv[i + 1], NULL, 16);
-                i++;
-            } else {
-                printf("--lic needs an argument\n");
-                return 1;
-            }
-        } else if (strcmp(argv[i], "--stereo") == 0) {
-            if (i + 1 < argc) {
-                set_carrier_volume(0, atoi(argv[i + 1]));
-                i++;
-            } else {
-                printf("--stereo needs an argument\n");
-                return 1;
-            }
-        } else if (strcmp(argv[i], "--rds") == 0) {
-            if (i + 1 < argc) {
-                set_carrier_volume(1, atoi(argv[i + 1]));
-                i++;
-            } else {
-                printf("--rds needs an argument\n");
-                return 1;
-            }
-        } else if (strcmp(argv[i], "--fifo") == 0) {
-            if (i + 1 < argc) {
-                memcpy(control_pipe, argv[i + 1], 50);
-                i++;
-            } else {
-                printf("--fifo needs an argument\n");
-                return 1;
-            }
+                rds_params.pi = strtoul(argv[i + 1], NULL, 16);
+            i++;
+        } else if (strcmp(argv[i], "--ps") == 0 && i + 1 < argc) {
+            memcpy(rds_params.ps, xlat((unsigned char *)argv[++i]), 8);
+        } else if (strcmp(argv[i], "--rt") == 0 && i + 1 < argc) {
+            memcpy(rds_params.rt, xlat((unsigned char *)argv[++i]), 64);
+        } else if (strcmp(argv[i], "--pty") == 0 && i + 1 < argc) {
+            rds_params.pty = strtoul(argv[++i], NULL, 10);
+        } else if (strcmp(argv[i], "--ptyn") == 0 && i + 1 < argc) {
+            memcpy(rds_params.ptyn, xlat((unsigned char *)argv[++i]), 8);
+        } else if (strcmp(argv[i], "--ms") == 0 && i + 1 < argc) {
+            rds_params.ms = strtoul(argv[++i], NULL, 10);
+        } else if (strcmp(argv[i], "--tp") == 0 && i + 1 < argc) {
+            rds_params.tp = strtoul(argv[++i], NULL, 10);
+        } else if (strcmp(argv[i], "--di") == 0 && i + 1 < argc) {
+            rds_params.di = strtoul(argv[++i], NULL, 10);
+        } else if (strcmp(argv[i], "--af") == 0 && i + 1 < argc) {
+            add_rds_af(&rds_params.af, strtof(argv[++i], NULL));
+        } else if (strcmp(argv[i], "--ecc") == 0 && i + 1 < argc) {
+            rds_params.ecc = strtoul(argv[++i], NULL, 16);
+        } else if (strcmp(argv[i], "--lic") == 0 && i + 1 < argc) {
+            rds_params.lic = strtoul(argv[++i], NULL, 16);
+        } else if (strcmp(argv[i], "--stereo") == 0 && i + 1 < argc) {
+            set_carrier_volume(0, atoi(argv[++i]));
+        } else if (strcmp(argv[i], "--rds") == 0 && i + 1 < argc) {
+            set_carrier_volume(1, atoi(argv[++i]));
+        } else if (strcmp(argv[i], "--fifo") == 0 && i + 1 < argc) {
+            strncpy(control_pipe, argv[++i], sizeof(control_pipe) - 1);
         } else if (strcmp(argv[i], "--help") == 0) {
             show_help();
-            return 1;
+            return 0;
         } else {
-            printf("\n");
-            printf(" Invalid option: %s", argv[i]);
-            printf("\n");
             show_help();
             return 1;
         }
     }
 
-    pthread_mutex_init(&control_pipe_mutex, NULL);
-    pthread_cond_init(&control_pipe_cond, NULL);
-    pthread_attr_init(&attr);
-
-    mpx_buffer = malloc(NUM_MPX_FRAMES_IN * 2 * sizeof(float));
+    mpx_buffer = malloc(NUM_MPX_FRAMES_IN  * 2 * sizeof(float));
     out_buffer = malloc(NUM_MPX_FRAMES_OUT * 2 * sizeof(float));
-    dev_out = malloc(NUM_MPX_FRAMES_OUT * 2 * sizeof(int16_t));
+    dev_out    = malloc(NUM_MPX_FRAMES_OUT * 2 * sizeof(int16_t));
+
+    if (!mpx_buffer || !out_buffer || !dev_out)
+        goto exit;
 
     signal(SIGINT, stop);
     signal(SIGTERM, stop);
@@ -251,47 +178,52 @@ int main(int argc, char **argv) {
     fm_mpx_init(MPX_SAMPLE_RATE);
     init_rds_encoder(rds_params);
 
-    memset(&format, 0, sizeof(struct ao_sample_format));
+    memset(&format, 0, sizeof(format));
     format.channels = 2;
     format.bits = 16;
     format.rate = OUTPUT_SAMPLE_RATE;
     format.byte_format = AO_FMT_LITTLE;
 
     ao_initialize();
-    device = ao_open_live(ao_default_driver_id(), &format, NULL);
-    if (device == NULL) {
-        fprintf(stderr, "Error: cannot open sound device\n");
-        ao_shutdown();
+    device = open_audio_device(&format);
+    if (!device)
         goto exit;
-    }
 
-    memset(&src_data, 0, sizeof(SRC_DATA));
-    src_data.input_frames = NUM_MPX_FRAMES_IN;
+    memset(&src_data, 0, sizeof(src_data));
+    src_data.input_frames  = NUM_MPX_FRAMES_IN;
     src_data.output_frames = NUM_MPX_FRAMES_OUT;
-    src_data.src_ratio = (double)OUTPUT_SAMPLE_RATE / (double)MPX_SAMPLE_RATE;
-    src_data.data_in = mpx_buffer;
+    src_data.src_ratio =
+        (double)OUTPUT_SAMPLE_RATE / (double)MPX_SAMPLE_RATE;
+    src_data.data_in  = mpx_buffer;
     src_data.data_out = out_buffer;
 
     src_state = src_new(SRC_LINEAR, 2, &r);
     if (!src_state) {
-        fprintf(stderr, "Error: resampler error - %s\n", src_strerror(r));
+        fprintf(stderr, "Resampler error: %s\n", src_strerror(r));
         goto exit;
     }
 
     if (control_pipe[0]) {
-        if (open_control_pipe(control_pipe) == 0) {
-            fprintf(stderr, "Reading control commands on '%s'...\n", control_pipe);
-            r = pthread_create(&control_pipe_thread, &attr, control_pipe_worker, NULL);
-            if (r < 0) {
-                fprintf(stderr, "Error: could not create control pipe thread\n");
-                control_pipe[0] = 0;
-                goto exit;
-            }
-        } else {
-            fprintf(stderr, "Error: failed to open control pipe '%s'\n", control_pipe);
-            control_pipe[0] = 0;
+        if (open_control_pipe(control_pipe) != 0) {
+            fprintf(stderr,
+                    "Error: failed to open control pipe '%s'\n",
+                    control_pipe);
             goto exit;
         }
+    
+        pthread_attr_init(&attr);
+        if (pthread_create(&control_thread, &attr,
+                           control_pipe_worker, NULL) != 0) {
+            fprintf(stderr,
+                    "Error: failed to create control pipe thread\n");
+            close_control_pipe();
+            goto exit;
+        }
+    
+        control_thread_running = 1;
+        fprintf(stderr,
+                "Reading control commands on '%s'...\n",
+                control_pipe);
     }
 
     while (!stop_rds) {
@@ -302,22 +234,31 @@ int main(int argc, char **argv) {
 
         float2char2channel(out_buffer, dev_out, frames);
 
-        if (!ao_play(device, dev_out, frames * 2 * sizeof(int16_t))) {
-            fprintf(stderr, "Error: audio write failure\n");
-            break;
+        if (!ao_play(device,
+                     dev_out,
+                     frames * 2 * sizeof(int16_t))) {
+
+            fprintf(stderr,
+                    "Audio write failed, reopening device...\n");
+
+            ao_close(device);
+            device = open_audio_device(&format);
+            if (!device)
+                break;
         }
     }
 
-    resampler_exit(src_state);
-
 exit:
-    if (control_pipe[0]) {
-        fprintf(stderr, "Waiting for pipe thread to shut down...\n");
-        pthread_cond_signal(&control_pipe_cond);
-        pthread_join(control_pipe_thread, NULL);
-    }
+    if (control_thread_running)
+        pthread_join(control_thread, NULL);
 
-    pthread_attr_destroy(&attr);
+    if (device)
+        ao_close(device);
+
+    ao_shutdown();
+
+    if (src_state)
+        resampler_exit(src_state);
 
     fm_mpx_exit();
     exit_rds_encoder();
